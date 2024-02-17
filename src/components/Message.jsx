f9b46590ad81cea264a8feaa548e8230eb50f822
toshi-Devs <trahim87mustapha@gmail.com>
2024-02-18 01:12:26 +0200

    design is done

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Message() {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+
+      <div className='messageInfo'>
+
+        <img src="https://pbs.twimg.com/profile_images/1467863408355815441/qP6IrLhF_400x400.jpg" alt="logo" />
+        <span>just now</span>
+      </div>
+
+      <div className="messageContent">
+
+        <p>Hello!</p>
+        <img src="https://pbs.twimg.com/profile_images/1467863408355815441/qP6IrLhF_400x400.jpg" alt="avatar" />
+
+      </div>
+
+    </div>
   )
 }
 
